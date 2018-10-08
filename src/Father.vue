0cda31fc92828db7e0af4725e98caf5363c7ddc1
Anoob Bava <anoob.bava@gmail.com>
2018-10-08 19:39:46 +0530

    added father component

---
+++ src/Father.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>this is from {{name}}</h3>
+  </div>
+</template>
+
+<script>
+  export default{
+    data: function(){
+      return {
+        name: 'father'
+      }
+    },
+    methods: {
+    },
+    components: {
+    }
+  }
+</script>
+
+<style scoped>
+
+  div{
+    background-color: yellow;
+  }
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Father from './Father.vue'
+Vue.component('father-component', Father)
 new Vue({
   el: '#app',
   render: h => h(App)
